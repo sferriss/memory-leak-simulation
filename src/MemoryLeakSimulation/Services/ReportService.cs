64757dcf70f8b0807d69f0493158d2deff405f43
Sandro <sandro_junior@hotmail.com>
2024-08-15 22:13:19 -0300

    frist

---
+++ src/MemoryLeakSimulation/Services/ReportService.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace MemoryLeakSimulation.Services;
+
+public static class ReportService
+{
+    private static readonly Dictionary<Guid, string> Results = new();
+
+    public static Guid Generate()
+    {
+        var requestId = Guid.NewGuid();
+
+        Task.Run(() =>
+        {
+            var sb = new StringBuilder();
+            for (var i = 0; i < 5000; i++)
+            {
+                var dateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                sb.Append($"This is a test: {dateTime}");
+            }
+
+            Results.Add(requestId, sb.ToString());
+            SaveResult(requestId, sb.ToString());
+
+        });
+
+        return requestId;
+    }
+    private static void SaveResult(Guid requestId, string result)
+    {
+        
+    }
+}
